139881e1b7a91a418ca4c76a2ec650604d9b204b
Schuyler Ankele <schuyler.ankele@gmail.com>
2018-09-23 02:20:51 -0600

    Starter for injectable JS functions.

---
+++ lineGraphCoords.js
@@ -0,0 +1,11 @@
+/* Line Graph Coordinates
+ * Injectable function for getting the plot points of an SVG line for Highcharts
+ *
+ */
+
+const centerOfLine =  (lineNumber = 0) => {
+    let x = 0;
+    let y = 0;
+
+    return `${x_coord} ${y_coord}`;
+};
